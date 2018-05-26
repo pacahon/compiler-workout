8a7d13de153a859a5f69b9d4f5883ff9a850d589
pacahon <rochon@yandex.ru>
2018-05-26 22:56:14 +0300

    prev progress

--- src/X86.ml
+++ src/X86.ml
@@ -93,7 +93,154 @@ open SM
    Take an environment, a stack machine program, and returns a pair --- the updated environment and the list
    of x86 instructions
 *)
-let compile env code = failwith "Not implemented"
+let compile env code =
+  let suffix = function
+  | "<"  -> "l"
+  | "<=" -> "le"
+  | "==" -> "e"
+  | "!=" -> "ne"
+  | ">=" -> "ge"
+  | ">"  -> "g"
+  | _    -> failwith "unknown operator"
+  in
+  let rec compile' env scode =
+    let on_stack = function S _ -> true | _ -> false in
+    let call env f n p =
+      let f =
+        match f.[0] with '.' -> "B" ^ String.sub f 1 (String.length f - 1) | _ -> f
+      in
+      let pushr, popr =
+        List.split @@ List.map (fun r -> (Push r, Pop r)) (env#live_registers n)
+      in
+      let env, code =
+        if n = 0
+        then env, pushr @ [Call f] @ (List.rev popr)
+        else
+          let rec push_args env acc = function
+            | 0 -> env, acc
+            | n -> let x, env = env#pop in
+              push_args env ((Push x)::acc) (n-1)
+          in
+          let env, pushs = push_args env [] n in
+          let pushs =
+            match f with
+            | "Belem" -> List.rev pushs
+            | "Barray" | "Bsexp" -> List.rev @@ (Push (L n)) :: pushs
+            | "Bsta" ->
+              let x::v::is = List.rev pushs in
+              is @ [x; v] @ [Push (L (n-2))]
+            | _  -> pushs
+          in
+          env, pushr @ pushs @ [Call f; Binop ("+", L (n*4), esp)] @ (List.rev popr)
+      in
+      (if p then env, code else let y, env = env#allocate in env, code @ [Mov (eax, y)])
+    in
+    let mov src dest =
+      match src, dest with
+      | R _, _ | _, R _ -> [Mov (src, dest)]
+      | _ -> [Mov (src, eax); Mov (eax, dest)]
+    in
+    (* Packing first 5 symbols of tag to int *)
+    let tag_to_int tag =
+      let len = String.length tag in
+      let prefix = String.sub tag 0 (if len < 5 then len else 5) in
+      let prefix_len = String.length prefix in
+      let ord = function
+        | '_' -> 53
+        |  ch  -> Char.code ch - (if ch > 'Z' then 70 else 64)
+      in
+      let rec pack acc n =
+        if n >= prefix_len
+        then acc
+        else pack ((acc lsl 6) lor (ord prefix.[n])) (n + 1)
+      in
+      pack 0 0
+    in
+    match scode with
+    | [] -> env, []
+    | instr :: scode' ->
+      let env', code' =
+        match instr with
+        | CONST n ->
+          let s, env' = env#allocate in
+          env', [Mov (L n, s)]
+        | STRING s ->
+          let s, env = env#string s in
+          let l, env = env#allocate in
+          let env, call = call env ".string" 1 false in
+          env, Mov (M ("$" ^ s), l) :: call
+        | SEXP (tag, n) ->
+          let env, call = call env ".sexp" (n + 1) false in
+          env, [Push (L (tag_to_int tag))] @ call
+        | LD x ->
+          let s, env' = (env#global x)#allocate in
+          env', mov (env'#loc x) s
+        | ST x ->
+          let s, env' = (env#global x)#pop in
+          env', mov s (env'#loc x)
+        | STA (x, n) ->
+          let s, env = (env#global x)#allocate in
+          let push = mov (env#loc x) s in
+          let env, code = call env ".sta" (n + 2) true in
+          env, push @ code
+        | LABEL s -> env, [Label s]
+        | JMP   l -> env, [Jmp l]
+        | CJMP (s, l) ->
+          let x, env = env#pop in
+          env, [Binop ("cmp", L 0, x); CJmp (s, l)]
+        | BEGIN (f, a, l) ->
+          let env = env#enter f a l in
+          env, [Push ebp; Mov (esp, ebp); Binop ("-", M ("$" ^ env#lsize), esp)]
+        | END ->
+          env, [Label env#epilogue;
+                Mov (ebp, esp);
+                Pop ebp;
+                Ret;
+                Meta (Printf.sprintf "\t.set\t%s,\t%d" env#lsize (env#allocated * word_size))]
+        | CALL (f, n, p) -> call env f n p
+        | RET b ->
+          if b
+          then let x, env = env#pop in env, [Mov (x, eax); Jmp env#epilogue]
+          else env, [Jmp env#epilogue]
+        | BINOP op -> (
+          let x, y, env' = env#pop2 in
+          env'#push y,
+          (match op with
+           | "/" | "%" -> [Mov (y, eax); Cltd; IDiv x; Mov ((match op with "/" -> eax | _ -> edx), y)]
+           | "<" | "<=" | "==" | "!=" | ">=" | ">" -> (
+              match x with
+              | M _ | S _ -> [Binop ("^", eax, eax); Mov (x, edx); Binop ("cmp", edx, y); Set (suffix op, "%al"); Mov (eax, y)]
+              | _ -> [Binop ("^"  , eax, eax); Binop ("cmp", x, y); Set (suffix op, "%al"); Mov (eax, y)]
+           )
+           | "*" ->
+             if on_stack x && on_stack y
+             then [Mov (y, eax); Binop (op, x, eax); Mov (eax, y)]
+             else [Binop (op, x, y)]
+           | "&&" ->
+             [Mov   (x, eax);
+              Binop (op, x, eax);
+              Mov   (L 0, eax);
+              Set   ("ne", "%al");
+              Mov   (y, edx);
+              Binop (op, y, edx);
+              Mov   (L 0, edx);
+              Set   ("ne", "%dl");
+              Binop (op, edx, eax);
+              Set   ("ne", "%al");
+              Mov   (eax, y)
+             ]
+           | "!!" -> [Mov (y, eax); Binop (op, x, eax); Mov (L 0, eax); Set ("ne", "%al"); Mov (eax, y)]
+           | _   ->
+             if on_stack x && on_stack y
+             then [Mov (x, eax); Binop (op, eax, y)]
+             else [Binop (op, x, y)]
+          )
+        )
+      in
+      let env'', code'' = compile' env' scode' in
+      env'', code' @ code''
+  in
+  compile' env code
 
 (* A set of strings *)           
 module S = Set.Make (String) 
@@ -104,7 +251,7 @@ module M = Map.Make (String)
 (* Environment implementation *)
 class env =
   let chars          = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNJPQRSTUVWXYZ" in
-  let make_assoc l i = List.combine l (List.init (List.length l) (fun x -> x + i)) in
+  let make_assoc l i = List.combine l (Language.list_init (List.length l) (fun x -> x + i)) in
   let rec assoc  x   = function [] -> raise Not_found | l :: ls -> try List.assoc x l with Not_found -> assoc x ls in
   object (self)
     val globals     = S.empty (* a set of global variables         *)
